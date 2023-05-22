0afaad9a54031dfedbb9ffbfa6cdbc979b028730
Vijay Madhu <vijay.madhu@primehealth.ae>
2023-05-22 16:34:46 +0530

    Test3

--- WpfApp1/MainWindow.xaml_BACKUP_708.cs
+++ WpfApp1/MainWindow.xaml_BACKUP_708.cs
@@ -1,1 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+<<<<<<< HEAD
+        //Test By Sowndaiyan
 =======
+
+        //Test By Sowndaiyan
+        //Test 2 By Sowndaiyan
+>>>>>>> 8a6703e7874c7165be9e7d4d2c84f6442dcd1974
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
